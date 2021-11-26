07496c905d774cc06f37019da2af0192ba0828a2
JAIMIT PANCHAL <jaimit.jp@gmail.com>
2021-11-26 23:57:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import { db, auth } from "./firebase-config";
+import firebase from 'firebase';
+import index from './index.css';
+import { useAuthState } from 'react-firebase-hooks/auth'
+
+
+
+
+// import {
+//   collection,
+//   getDocs,
+//   addDoc,
+//   updateDoc,
+//   deleteDoc,
+//   doc,
+//   docs,
+// } from "firebase/firestore";
+
 
 function App() {
+  const [newName, setNewName] = useState("");
+  const [newAge, setNewAge] = useState(0);
+  const [users, setUsers] = useState([]);
+  const [user] = useAuthState(auth)
+
+  // const { uid, photoURL } = auth.currentUser;
+
+  //   console.log("auth or user does not exist")
+
+
+  // const usersCollectionRef = collection(db, "users");
+
+
+  const getUsers = async () => {
+    await db.collection('users').onSnapshot(snapshot => {
+      setUsers(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    });
+  };
+
+  const signInWithGoogle = async () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider)
+  };
+
+  const createUser = async () => {
+    const { uid, photoURL } = auth.currentUser;
+
+
+    await db.collection('users').add({
+      'name': newName,
+      'age': Number(newAge),
+      photoURL,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp()
+    }).then(() => {
+
+    });
+
+    setNewName('');
+    setNewAge(0);
+    // await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) });
+    // await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) });
+  };
+
+  const updateUser = async (id, age) => {
+
+    await db.collection('users').doc(id).update({
+      'age': '20',
+      'name': 'Updated Name'
+    }).then(() => {
+
+    });
+  };
+
+  const deleteUser = async (id) => {
+    await db.collection('users').doc(id).delete().then(() => {
+    });
+  };
+
+  //this will render as the screen loads 
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        user ? <div className="App">
+          <input
+            value={newName}
+            placeholder="Name..."
+            onChange={(event) => {
+              setNewName(event.target.value);
+            }}
+          />
+          <input
+            value={newAge}
+            type="number"
+            placeholder="Age..."
+            onChange={(event) => {
+              setNewAge(event.target.value);
+            }}
+          />
+          <button onClick={createUser}> Create User</button>
+          {
+            users ?
+              <> <button className="logoutButton" onClick={() => auth.signOut()}> Sign Out</button></>
+              :
+              <button className="registerButton" onClick={signInWithGoogle}> Sign in your google account</button>
+          }
+
+          {/* <img src={photoURL} alt="Image Not Found" /> */}
+          {users.map((user) => {
+            return (
+              <div>
+                {" "}
+              <p>
+                <img className="imgdata" src={user.photoURL} alt="" />
+              </p>
+                <h1>Name: {user.name}</h1>
+                <h1>Age: {user.age}</h1>
+                <button
+                  onClick={() => {
+                    updateUser(user.id, user.age);
+                  }}
+                >
+                  {" "}
+                  Increase Age
+                </button>
+                <button
+                  onClick={() => {
+                    deleteUser(user.id);
+                  }}
+                >
+                  {" "}
+                  Delete User
+                </button>
+              </div>
+            );
+          })}
+        </div> : <div> User Does not Exist
+          <button className="registerButton" onClick={signInWithGoogle}> Sign in your google account</button>
+        </div>
+      }
+    </>
   );
 }
 
---
+++ src/firebase-config.js
@@ -0,0 +1,46 @@
+//old method
+import firebase from 'firebase'
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyC9YV7mF9PcwYkCEyFyU6a-PdOl3HUWTPg",
+  authDomain: "websitefirebase-ca9f3.firebaseapp.com",
+  databaseURL: "https://websitefirebase-ca9f3.firebaseio.com",
+  projectId: "websitefirebase-ca9f3",
+  storageBucket: "websitefirebase-ca9f3.appspot.com",
+  messagingSenderId: "468542671800",
+  appId: "1:468542671800:web:70e3b62757d23cb248dd4d",
+  measurementId: "G-57PPQ18TQK"
+});
+
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+
+export { db, auth }
+
+//new method
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "@firebase/firestore";
+// // import firebase from 'firebase';
+// // import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC9YV7mF9PcwYkCEyFyU6a-PdOl3HUWTPg",
+//   authDomain: "websitefirebase-ca9f3.firebaseapp.com",
+//   databaseURL: "https://websitefirebase-ca9f3.firebaseio.com",
+//   projectId: "websitefirebase-ca9f3",
+//   storageBucket: "websitefirebase-ca9f3.appspot.com",
+//   messagingSenderId: "468542671800",
+//   appId: "1:468542671800:web:70e3b62757d23cb248dd4d",
+//   measurementId: "G-57PPQ18TQK"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// // const analytics = getAnalytics(app);
+
+// export const db = getFirestore(app);
